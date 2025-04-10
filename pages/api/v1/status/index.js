8c9595df896500e12c5313d80a7125585abe0629
Dyslexic <michelcabral@gmail.com>
2025-04-10 04:38:28 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,7 @@
+import database from "../../../../infra/database.js";
+
 export default async function (request, response) {
+  const result = await database.query("SELECT 1 + 1 as result;");
+  console.log(result.rows);
   response.status(200).json({ status: "running" });
 }
