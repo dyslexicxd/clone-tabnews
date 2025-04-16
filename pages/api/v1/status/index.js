fb7637a0191394bf7608aeb7de7916cd231699e0
Dyslexic <michelcabral@gmail.com>
2025-04-16 22:40:41 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,22 @@
 import database from "infra/database.js";
 
 export default async function (request, response) {
-  const result = await database.query("SELECT 1 + 1 as result;");
-  console.log(result.rows);
-  response.status(200).json({ status: "running" });
+  const updatedAt = new Date().toISOString();
+  const dbVersion = await database.query("SHOW server_version;");
+  const maxConnections = await database.query("SHOW max_connections;");
+  const openedConnections = await database.query({
+    text: "SELECT count(*)::int AS opened_connections FROM pg_stat_activity WHERE datname = $1;",
+    values: [process.env.POSTGRES_DB],
+  });
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersion.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        opened_connections: openedConnections.rows[0].opened_connections,
+      },
+    },
+  });
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,19 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  console.log(responseBody);
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  expect(responseBody.dependencies.database.version).toBe("16.8");
+  expect(responseBody.dependencies.database.max_connections).not.toBeNaN();
+  expect(
+    responseBody.dependencies.database.max_connections,
+  ).toBeGreaterThanOrEqual(0);
+  expect(responseBody.dependencies.database.opened_connections).not.toBeNaN();
+  expect(
+    responseBody.dependencies.database.opened_connections,
+  ).toBeGreaterThanOrEqual(0);
 });
