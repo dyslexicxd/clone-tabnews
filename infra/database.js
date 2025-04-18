ddaaa3b9263cc29994f228ee59364071b2897f30
Dyslexic <michelcabral@gmail.com>
2025-04-18 20:22:38 +0000

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -6,6 +6,7 @@ const pool = new Pool({
   user: process.env.POSTGRES_USER,
   database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASSWORD,
+  ssl: process.env.NODE_ENV != "development",
 });
 
 async function query(queryObject) {
